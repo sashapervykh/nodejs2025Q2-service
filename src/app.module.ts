b46137ebf5bc9c717545bd7a9f023af47ebbae11
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-11-25 17:42:20 +0300

    feat: create database structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './modules/user/user.controller';
-import { UserService } from './modules/user/user.service';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [UserModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,3 +1,11 @@
-export class Database {}
+import { User } from 'src/modules/user/user.interface';
+
+export class Database {
+  users: User[] = [];
+  favorites: User[] = [];
+  albums: User[] = [];
+  artists: User[] = [];
+  tracks: User[] = [];
+}
 
 export const database = new Database();
