72ecd2da6eaa3c0c1320f879039c9aae2dcac7c9
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-11-26 12:13:51 +0300

    refactor: remove unnecessary folders

--- src/modules/users/user.dto.ts
+++
@@ -1,9 +0,0 @@
-export interface CreateUserDto {
-  login: string;
-  password: string;
-}
-
-export interface UpdatePasswordDto {
-  oldPassword: string; // previous password
-  newPassword: string; // new password
-}
